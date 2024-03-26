e7e5ad9dd7de7dbf9ac902791d22b0a2a2d699bd
Leticia.k <dmswjd_14@naver.com>
2024-03-26 16:55:51 +0900

    Composition API 템플릿 문법

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,13 +1,16 @@
+<!-- Composition API 방식-->
 <template>
-  <ul>
-    <h2 v-for="item in items" v-bind:key="item.name">
-      {{ item.name }}의 가격은 {{ item.price }}원이고, 재고는 {{ item.quantity }} 남아있다.  
-    </h2>
-</ul>
+  <div id="app">
+    <ul>
+      <li v-for="item in items" :key="item.name">
+        {{ item.name }}의 가격은 {{ item.price }}원이고, 재고는 {{ item.quantity }}개 남아있다.
+      </li>
+    </ul>
+  </div>
 </template>
 
 <!-- Composition API 방식-->
-<!-- <script>
+<script>
 // ref 컴포지션 함수 import
 import {ref} from 'vue'
 
@@ -32,21 +35,5 @@ export default{
     }
   },
 }
-</script> -->
-
-
-<!-- Options API 방식 -->
-<script>
-export default {
-  data() {
-    return {
-      message: "Hello",
-      items: [
-        { name: '일번', price: 1000, quantity: 111 },
-        { name: '이번', price: 2000, quantity: 222 },
-        { name: '삼번', price: 3000, quantity: 333 },
-      ],
-    };
-  },
-};
 </script>
+
