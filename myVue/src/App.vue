493b3bd72246be3f25dca021d3ce27b14164cf70
Leticia.k <dmswjd_14@naver.com>
2024-03-26 16:45:10 +0900

    Vue템플릿 문법(Composition, Options)
    - 템플릿은 vue 인스턴스의 데이터와 뷰의 관계를 선언적으로 정의하는 역할을 한다.
    - 템플릿 문법에는 "Mustache"와 "v-bind" 속성이 있다.
    - [ v-bind ]
      - 아이디, 클래스, 스타일 등의 html 속성 값에 뷰 데이터 값을 연결할 때 사용한다.
      - 형식은 v-bind 속성으로 지정할 html 속성 앞에 접두사로 v-bind: 를  붙여준다.
      - v-bind: 문법을 :로 간소화 할 수 있다.
    
    - [ Mustache ]
      - 뷰 인스턴스의 데이터를 html 태그에 연결하여 텍스트를 삽입하는 방식이다.
      - 다른 언어나 프레임워크에서도 자주 사용되는 문법으로 {{ }}안에 JS 표현식을 넣을 수 있다.

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,36 +1,52 @@
 <template>
-  <h1>{{ message }}</h1>
+  <ul>
+    <h2 v-for="item in items" v-bind:key="item.name">
+      {{ item.name }}의 가격은 {{ item.price }}원이고, 재고는 {{ item.quantity }} 남아있다.  
+    </h2>
+</ul>
 </template>
 
-<script>
-import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
-export default {
-  setup() {
-    const message = "Hello Vue!"
+<!-- Composition API 방식-->
+<!-- <script>
+// ref 컴포지션 함수 import
+import {ref} from 'vue'
 
-    // 인스턴스가 마운트되기 전 단계
-    onBeforeMount(() => {
-      console.log("beforeMount\n");
-    })
-    
-    // 인스턴스가 마운트된 후 단계
-    onMounted(() => {
-      console.log("mounted\n");
-    })
-    
-    // 데이터가 변경되어 DOM에 적용되기 전 단계
-    onBeforeUpdate(() => {
-      console.log("beforeUpdate\n");
-    })
+export default{
+  setup(){
+    const message = ref("Hello")
+    const items = ref([
+      {
+        name: '일번', price: 100, quantity: 111
+      },
+      {
+        name: '이번', price: 200, quantity: 222
+      },
+      {
+        name: '삼번', price: 300, quantity: 333
+      },
+    ])
 
-    // 데이터가 변경되어 DOM에 적용된 후 단계
-    onUpdated(() => {
-      console.log("updated\n");
-    })
-
-    return {
-      message,  
+    return{
+      message,
+      items,
     }
   },
 }
+</script> -->
+
+
+<!-- Options API 방식 -->
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello",
+      items: [
+        { name: '일번', price: 1000, quantity: 111 },
+        { name: '이번', price: 2000, quantity: 222 },
+        { name: '삼번', price: 3000, quantity: 333 },
+      ],
+    };
+  },
+};
 </script>
