42a5d61c5a90f459b407503c12ac7bac8197f86c
Leticia.k <dmswjd_14@naver.com>
2024-03-26 17:47:18 +0900

    [ Options API ] - computed 속성
    v-model 대신 :value와 @input을 사용하여 각 아이템의 가격을 양방향으로 바인딩
    
    사용자가 입력 필드에 값을 입력할 때 updatePrice 메서드가 호출되어 해당 아이템의 가격을 업데이트

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,8 +1,9 @@
-<!-- Composition API  -->
+<!-- Options API  -->
 <template>
   <ul>
     <li v-for="item in items" v-bind:key="item.name">
-      {{ item.name }}의 가격: <input type="text" v-model="item.price">
+      <!-- {{ item.name }}의 가격: <input type="text" v-on:input="item.price = $event.target.value" v-bind:value="item.price"> -->
+      {{ item.name }}의 가격: <input type="text" :value="item.price" @input="updatePrice(item, $event)">
     </li> 
   </ul> 
   <br>
@@ -21,33 +22,32 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue'
-
 export default {
-  setup() {
-    const items = ref([
-      {
-        name: 'CPU', price: 462984, quantity: 1
-      }, 
-      {
-        name: '메인보드', price: 112053, quantity: 1
-      }, 
-      {
-        name: '메모리', price: 79608, quantity: 2
-      }
-    ])
-    
-    // computed 속성
-    const totalPrice = computed(() => {
-      return items.value.reduce(function (sum, item) {
-        return sum + (item.price * item.quantity)
-      }, 0)
-    })
 
+  data() {
     return {
-      items,
-      totalPrice,
+      items: [
+        { name: 'CPU', price: 462984, quantity: 1 },
+        { name: '메인보드', price: 112053, quantity: 1 },
+        { name: '메모리', price: 79608, quantity: 2 }
+      ],
+    };
+  },
+
+  computed: {
+    // computed 속성을 사용하여 총합 계산
+    totalPrice() {
+      return this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
     }
   },
-}
+
+  methods: {
+    // 입력 필드를 통해 price를 업데이트하는 메소드
+    updatePrice(item, event) {
+      // 입력된 값을 숫자로 변환하여 item.price에 할당
+      item.price = Number(event.target.value);
+    }
+  }
+  
+};
 </script>
