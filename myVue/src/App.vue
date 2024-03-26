5ded01ca8e93012cac0b42f9caceeb25bf27aeed
Leticia.k <dmswjd_14@naver.com>
2024-03-26 16:14:12 +0900

    Composition API 라이프 사이클
    onBeforeMount - onMounted -  onBeforeUpdate - onUpdated

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -3,11 +3,33 @@
 </template>
 
 <script>
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
 export default {
-  setup(){
-    const message = "Hello, Vue!"
-    return { 
-      message, 
+  setup() {
+    const message = "Hello Vue!"
+
+    // 인스턴스가 마운트되기 전 단계
+    onBeforeMount(() => {
+      console.log("beforeMount\n");
+    })
+    
+    // 인스턴스가 마운트된 후 단계
+    onMounted(() => {
+      console.log("mounted\n");
+    })
+    
+    // 데이터가 변경되어 DOM에 적용되기 전 단계
+    onBeforeUpdate(() => {
+      console.log("beforeUpdate\n");
+    })
+
+    // 데이터가 변경되어 DOM에 적용된 후 단계
+    onUpdated(() => {
+      console.log("updated\n");
+    })
+
+    return {
+      message,  
     }
   },
 }
