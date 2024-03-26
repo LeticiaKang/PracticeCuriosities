c0a3953471fbab1ba4fa518ae5328887a008c90b
Leticia.k <dmswjd_14@naver.com>
2024-03-26 16:56:06 +0900

    Options API 템플릿 문법

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,39 +1,28 @@
-<!-- Composition API 방식-->
+<!-- Options API -->
 <template>
   <div id="app">
     <ul>
+      <!-- v-bind 속성-->
       <li v-for="item in items" :key="item.name">
+        <!-- 아이디, 클래스, 스타일 등의 HTML 속성 값에 뷰 데이터 값을 연결 -->
         {{ item.name }}의 가격은 {{ item.price }}원이고, 재고는 {{ item.quantity }}개 남아있다.
       </li>
     </ul>
   </div>
 </template>
 
-<!-- Composition API 방식-->
+<!-- Options API 방식 -->
 <script>
-// ref 컴포지션 함수 import
-import {ref} from 'vue'
-
-export default{
-  setup(){
-    const message = ref("Hello")
-    const items = ref([
-      {
-        name: '일번', price: 100, quantity: 111
-      },
-      {
-        name: '이번', price: 200, quantity: 222
-      },
-      {
-        name: '삼번', price: 300, quantity: 333
-      },
-    ])
-
-    return{
-      message,
-      items,
-    }
+export default {
+  data() {
+    return {
+      message: "Hello",
+      items: [
+        { name: '일번', price: 1000, quantity: 111 },
+        { name: '이번', price: 2000, quantity: 222 },
+        { name: '삼번', price: 3000, quantity: 333 },
+      ],
+    };
   },
-}
+};
 </script>
-
