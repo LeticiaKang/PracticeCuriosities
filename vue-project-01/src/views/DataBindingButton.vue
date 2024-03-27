b4efd5331454ba4f455096f3629b3bc0ec8b9c22
Leticia.k <dmswjd_14@naver.com>
2024-03-27 13:53:15 +0900

    [프로젝트 투입 일주일 전] 5장. 데이터 바인딩
    - 각 태그 및 타입별 데이터 바인딩 방법
    - 버튼 비활성화 사용 방법
    - 데이터 입력 후 배열로 다시 화면에 보여줌

--- vue-project-01/src/views/DataBindingButton.vue
+++ vue-project-01/src/views/DataBindingButton.vue
@@ -4,6 +4,15 @@
     <button type="button" v-bind:disabled="textValue==''">Click</button>
     <p>입력을 해야 버튼이 활성화된다.</p>
   </div>
+  <br>
+  <br>
+  <div>
+    <input v-model="newItem" placeholder="데이터 입력" />
+    <button @click="addItem" v-bind:disabled="newItem==''">Click</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,7 +20,17 @@ export default {
   data() {
     return {
       textValue: "",
+      newItem: '', // 사용자 입력을 위한 새 항목
+      items: [] // 입력된 모든 데이터를 담는 배열
     }
   },
+  methods: {
+    addItem() {
+      if (this.newItem !== '') {
+        this.items.push(this.newItem); // 새 항목을 배열에 추가
+        this.newItem = ''; // 입력 필드를 다시 비움
+      }
+    }
+  }
 }
 </script>
